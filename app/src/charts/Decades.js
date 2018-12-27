a94db112691edd8cce394f1d4dc3df71e9f93704
Filip <placheta.filip@gmail.com>
2018-12-28 00:55:20 +0100

    decades chart added

--- app/src/charts/Decades.js
+++ app/src/charts/Decades.js
@@ -1,13 +1,25 @@
+import _ from 'lodash';
 import React, { Component } from 'react';
 import Plot from 'react-plotly.js';
 
 export default class Decades extends Component {
+
+    prepareData() {
+        return _.chain(this.props.data)
+            .map('release_date')
+            .map(o => _.round(new Date(o).getFullYear(), -1))
+            .groupBy(o => o)
+            .mapValues('length')
+            .value();
+    }
+
     render() {
+        const data = this.prepareData();
         return <Plot
           data={[
             {
-              values: [1, 2, 3],
-              labels: ['a', 'b', 'c'],
+              values: _.values(data),
+              labels: _.keys(data),
               type: 'pie'
             }
           ]}
